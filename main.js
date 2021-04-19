e70d2726576060f1b159bd3df8aaf64dbf83eea2
Steve <callasteven@gmail.com>
2021-04-19 16:31:40 -0600

    Add inital draft of select character button and dropdown menu

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 // variables for querySelectors below
 let cards = document.querySelector('#box');
+let dropDownList = document.querySelector('#populateCheckbox');
+let selectCharacters = document.querySelector('#selectCharacters');
+let renderCheckBoxMenu = document.querySelector('#checkboxMenu');
 
 // global variables below
 
 // event listeners below
 window.addEventListener('load', loadCards);
+selectCharacters.addEventListener('click', toggleCharacterMenu);
+
 
 //functions below
 function loadCards() {
   createCharacterCards();
-  // test3();
+  createCharacterDropDownList();
+  //   test3();
+  //   setTimeout(() => {
+    //     console.log("second")
+    //     createCharacterCards();
+    // }, 1000)
+  }
+  
+function toggleCharacterMenu() {
+  renderCheckBoxMenu.classList.toggle('show');
 }
 
 function createCharacterCards() {
@@ -77,10 +91,11 @@ function renderCharacterCards(renderCards) {
   //------------------------------ CODE WORKS TO GRAB SPECIFIC CHARACTERS BUT CAN'T GET THE TESTARRAY TO FILL IN THE CREATE CARDS FUNCTION ----
 
 //   let testArray = []; 
+//   let abc;
 
 //   function test7(response) {
 //     console.log('test7', response)
-//     const a = response.data.results.map(character => (
+//     abc = response.data.results.map(character => (
 //       {name: character.name, 
 //         image: character.thumbnail.path, 
 //         desc: character.description, 
@@ -88,15 +103,16 @@ function renderCharacterCards(renderCards) {
 //         comics: character.comics.available , 
 //         stories: character.stories.available, 
 //       }));
-//     testArray.push(a);
-//     console.log('testArray=', testArray);
-//     createCharacterCards(testArray);
+//     testArray.push(abc);
+//     console.log(testArray);
+//     return testArray;
+//     // createCharacterCards(testArray);
 //   };
 //   // test5();
   
 //    function test3() {
 //     // const test = ['Falcon', 'Spider-Man', 'Black Widow', 'Adam Warlock', 'Agent Zero', '3-D Man', 'Absorbing Man', 'Spider-Girl (May Parker)', 'Spider-Ham (Larval Earth)', 'Spider-Woman (Mattie Franklin)'];
-//     const test = ['Falcon'];
+//     const test = ['Spider-Girl (May Parker)', 'Black Widow'];
 //     for (let i = 0; i < test.length; i++) {
 //       let abc = test2(test[i]);
 //     }
@@ -106,16 +122,16 @@ function renderCharacterCards(renderCards) {
 //   function test2(help) {
 //     fetch(`https://gateway.marvel.com:443/v1/public/characters?name=${help}&limit=1&ts=1&apikey=e2deecaf6c770a3c085bbc7ed4b93986&hash=5f76c2f28fdd90fe55091d98e6de3f43`)
 //     .then(response => response.json())
-//     // .then(response => response.data.results.map(character => ({name: character.name, image: character.thumbnail.path, desc: character.description, urls: character.urls, comics: character.comics.available , stories: character.stories.available, })))
+//     //.then(response => response.data.results.map(character => ({name: character.name, image: character.thumbnail.path, desc: character.description, urls: character.urls, comics: character.comics.available , stories: character.stories.available, })))
 //     .then(response => test7(response))
 //     .then(character => console.log('tag1=', character));
 //   }
 
-//   function createCharacterCards(testArray) {
+//   function createCharacterCards() {
 //     let characterCards = '';
-//     console.log('cards=', testArray);
-//     console.log('cards 0=', testArray[0]);
-//     console.log('cards length=', testArray.length);
+//     console.log('cards1=', testArray);
+//     // console.log('cards 0=', testArray[0]);
+//     // console.log('cards length=', testArray.length);
 //     for (let i = 0; i < testArray.length; i++) {
 //       characterCards += 
 //         `
@@ -124,22 +140,59 @@ function renderCharacterCards(renderCards) {
 //           <div class='image-box'>
 //             <div class='text-overlay-1'>VIEWS</div>
 //             <div class='text-overlay-2'>04</div>
-//             <img class='container-image' src ="${testArray[i].image_url}"
-//               alt="${testArray[i].name}">
+//             <img class='container-image' src ="${testArray[i][0].image}/landscape_medium.jpg"
+//               alt="${testArray[i][0].name}">
 //           </div>
-//           <h2 class='name-text'>${testArray[i].name}</h2>
-//           <p class='description-text'>${testArray[i].desc}</p>
-//           <a class='text url-box' href="${testArray[i].urls[0].url}" target="_blank">Link to profile</a>
-//           <a class='text url-box' href="${marvelCharacters[i].urls[1].url}" target="_blank">Link to comics</a>
-//           <p class='count-text'>Comic Count: ${testArray[i].comics}</p>
-//           <p class='count-text'>Stories Count: ${testArray[i].stories}</p>
+//           <h2 class='name-text'>${testArray[i][0].name}</h2>
+//           <p class='description-text'>${testArray[i][0].desc}</p>
+//           <a class='text url-box' href="${testArray[i][0].urls[0].url}" target="_blank">Link to profile</a>
+//           <a class='text url-box' href="${testArray[i][0].urls[0].url}" target="_blank">Link to comics</a>
+//           <p class='count-text'>Comic Count: ${testArray[i][0].comics}</p>
+//           <p class='count-text'>Stories Count: ${testArray[i][0].stories}</p>
 //         </article>  
 //         `;
 //     }
 //     renderCharacterCards(characterCards);
-//     console.log(characterCards);
+//     // console.log('cards2=', characterCards);
 //   }
 
 // function renderCharacterCards(renderCards) {
 //   cards.innerHTML = renderCards;
 // }
+
+function createCharacterDropDownList() {
+  // const test = ['Spider-Girl (May Parker)', 'Black Widow'];
+  var list = '';
+  for (let i = 0; i < marvelCharacters.length; i++) {
+    list += 
+      `
+      <input type="checkbox" id="coding" name="interest" value="coding">
+      <label for="coding">${marvelCharacters[i].name}</label>
+      <br>
+      `;
+  }
+  renderDropdownList(list);
+}
+
+function renderDropdownList(list) {
+  dropDownList.innerHTML = list;
+}
+
+{/* <input type="checkbox" id="scales" name="scales">
+<label for="scales">${marvelCharacters[i].name}</label>
+<br> */}
+
+{/* <form>
+  <fieldset>
+  <legend>Choose your interests</legend>
+    <div>
+      <input type="checkbox" id="coding" name="interest" value="coding">
+      <label for="coding">Coding</label>
+    </div>
+    <div>
+      <button type="submit">Submit form</button>
+    </div>
+  </fieldset>
+</form> */}
+
+
